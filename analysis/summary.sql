7c64589de1397743a37355efc418edb3d3913528
Derek Flenniken <vhasfcflennd@SFC-LT313271M.local>
2014-04-17 22:25:10 -0700

    Breakdown test completion chart into questionnaires and tests (cogstate, lumosity)

--- analysis/summary.sql
+++ analysis/summary.sql
@@ -7,6 +7,16 @@ SELECT
     , Family_Members_has_Alzheimer__c
     , t.TestsCompleted
     , t.CompletionRate
+    , t.QuestionnairesCompleted
+    , t.QuestionnairesCompletedPct
+    , t.CogStateCompleted
+    , t.LumosityCompleted
+    , CASE
+            WHEN t.CogStateCompleted > 0 AND t.LumosityCompleted > 0 THEN 'Both'
+            WHEN t.CogStateCompleted = 0 AND t.LumosityCompleted > 0 THEN 'Lumosity'
+            WHEN t.CogStateCompleted > 0 AND t.LumosityCompleted = 0 THEN 'CogState'
+            ELSE 'None'
+        END AS TestStatus
     , f.HowEasy
     , f.HowClear
     , f.HowLikely
@@ -17,6 +27,10 @@ LEFT OUTER JOIN
     SELECT
       c.Id
       , SUM(CASE WHEN qr.Status__c = 'Completed' THEN 1 ELSE 0 END) AS TestsCompleted
+      , MAX(CASE WHEN qr.Status__c = 'Completed' AND s.Name = 'Card Cognitive Test' THEN 1 ELSE 0 END) AS CogStateCompleted
+      , MAX(CASE WHEN qr.Status__c = 'Completed' AND s.Name = 'Lumosity Test' THEN 1 ELSE 0 END) AS LumosityCompleted
+      , SUM(CASE WHEN qr.Status__c = 'Completed' AND s.Name NOT IN ('Lumosity Test', 'Card Cognitive Test') THEN 1 ELSE 0 END) AS QuestionnairesCompleted
+      , CAST(CAST(SUM(CASE WHEN qr.Status__c = 'Completed' AND s.Name NOT IN ('Lumosity Test', 'Card Cognitive Test') THEN 1 ELSE 0 END) AS FLOAT)/CAST(10 AS FLOAT) AS DEC(3,2)) AS QuestionnairesCompletedPct
       , CAST(CAST(SUM(CASE WHEN qr.Status__c = 'Completed' THEN 1 ELSE 0 END) AS FLOAT)/CAST(COUNT(1) AS FLOAT) AS DEC(3,2)) AS CompletionRate
     FROM
       Questionnaire_Response__c qr
