6eaebe6bcbc3e2bfdd67eb589df58d1523970c56
Shannon Buckley <shannon.buckley@ucsf.edu>
2014-09-16 12:01:19 -0700

    Moving queries out of schema folder into 'queries'. Adding SFCountsByZip, which summarizes the zips with people 60+ yrs.

---
+++ database/queries/SF_CountsByZip.sql
@@ -0,0 +1,26 @@
+SELECT alias.zip, COUNT(1) as ZipN, ROUND(AVG(alias.Age__c), 2) AS AvgAge, ROUND(STDEV(alias.Age__c), 2) AS 'SD'
+FROM ( SELECT
+		  k.SubjectCode
+		  , z.zip
+		  , c.Age__c
+		  , z.county
+		  , z.state
+
+		FROM
+		  CodeKey k
+		INNER JOIN
+		  SF.Contact c
+		ON
+		  k.SalesforceID = c.Id
+		  AND IsDeleted = 'false'
+		LEFT OUTER JOIN
+		  ZipCodes z
+		ON
+		  c.MailingPostalCode = z.zip
+		WHERE
+			Age__c >= 60
+			AND 
+			z.county like '%san francisco%'
+		  ) alias
+
+GROUP By alias.zip
