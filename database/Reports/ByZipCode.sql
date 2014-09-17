573f1b3499e8b83fd62dd3552d46cdfce2fdabb0
Derek Flenniken <derek.flenniken@ucsf.edu>
2014-09-17 12:10:54 -0700

    Updated to use zipcode data with higher resolution lat/long

--- database/Reports/ByZipCode.sql
+++ database/Reports/ByZipCode.sql
@@ -1,11 +1,11 @@
 
-CREATE VIEW
+ALTER VIEW
   Reports.ByZipCode
 AS
 SELECT 
-  z.zip
-  , MIN(z.latitude) AS lat
-  , MIN(z.longitude) AS long
+  z.ZipCode AS zip
+  , MIN(z.lat) AS lat
+  , MIN(z.lon) AS long
   , REPLACE(MIN(z.county),' County','') AS county
   , MIN(z.state) AS state
   , COUNT(1) AS n
@@ -17,11 +17,12 @@ ON
   k.SalesforceID = c.Id
   AND IsDeleted = 'false'
 LEFT OUTER JOIN
-  ZipCodes z
+  zips z
 oN
-  c.MailingPostalCode = z.zip
+  c.MailingPostalCode = z.ZipCode
+  
 GROUP BY
-  z.zip
+  z.ZipCode
 
 
 
