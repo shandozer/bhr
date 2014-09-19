76f1e92ef901718298865df22f74fceaef2cb618
Derek Flenniken <derek@enth.net>
2014-09-18 19:02:12 -0700

    Update for migrated system.

--- dashboard/growth.sql
+++ dashboard/growth.sql
@@ -1,17 +1,29 @@
+
+
+CREATE VIEW
+	Reports.Growth
+AS
+
 WITH Daily(Day, N)
 AS (
     SELECT
-        date(CreatedDate) AS Day
+        CAST(RegistrationDateTime AS DATE) AS Day
         , COUNT(1) AS N
     FROM
-        Contact
-    GROUP BY
-        date(CreatedDate)
+        Subjects s
+	INNER JOIN
+		Studies st
+	ON
+		s.StudyID = st.ID
+	WHERE
+		st.Code = 'BHR'
+    GROUP BY 
+        CAST(RegistrationDateTime AS DATE)
 )
 SELECT
-Day
-, ( SELECT SUM(N) FROM Daily WHERE Day <= d.Day) AS TotalN
-, N AS DailyN
+	Day
+	, ( SELECT SUM(N) FROM Daily WHERE Day <= d.Day) AS TotalN
+	, N AS DailyN
 FROM
-Daily d
-;
+	Daily d
+
